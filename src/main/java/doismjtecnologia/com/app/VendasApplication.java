22dd97d640770a4fda1ae4e0ee7f8d12e46d70b7
Marcos Silva <silvasocram@gmail.com>
2022-06-28 13:47:34 -0300

    Merge with IntelliJ project

--- src/main/java/doismjtecnologia/com/app/VendasApplication.java
+++ src/main/java/doismjtecnologia/com/app/VendasApplication.java
@@ -2,10 +2,18 @@ package doismjtecnologia.com.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class VendasApplication {
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String helloWorld(){
+        return "Hello World this my first app using String Boot.";
     }
 }
