18944494e93e38aee928161b4e32c6eeae7170b9
Marcos Silva <silvasocram@gmail.com>
2022-07-04 13:50:50 -0300

    Creating data.sql file.

--- src/main/java/doismjtecnologia/com/app/VendasApplication.java
+++ src/main/java/doismjtecnologia/com/app/VendasApplication.java
@@ -1,8 +1,5 @@
 package doismjtecnologia.com.app;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
