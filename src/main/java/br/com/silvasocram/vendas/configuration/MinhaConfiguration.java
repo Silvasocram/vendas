d0ace9d25b8d9ae4fb716a1b2646ec00923ddbd2
Marcos Silva <silvasocram@gmail.com>
2022-10-10 15:36:59 -0300

    Outra forma de usar o CommandLineRunner.

--- src/main/java/br/com/silvasocram/vendas/configuration/MinhaConfiguration.java
+++ src/main/java/br/com/silvasocram/vendas/configuration/MinhaConfiguration.java
@@ -2,18 +2,21 @@ package br.com.silvasocram.vendas.configuration;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
 @Profile("development")
-public class MinhaConfiguration implements CommandLineRunner {
+public class MinhaConfiguration {
 
     @Value("${application.outra.message}")
     private String message;
 
-    @Override
-    public void run(String... args) throws Exception {
-        System.out.println(message);
+    @Bean
+    public CommandLineRunner executar(){
+        return args -> {
+            System.out.println(message);
+        };
     }
 }
